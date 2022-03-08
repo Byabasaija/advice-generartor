f827ec2d030c3f4ce9fe914d5d001444f1399fdb
Byabasaija <basaijapascal9@gmail.com>
2022-03-08 06:38:12 +0300

    finish up

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const apiUrl = 'https://api.adviceslip.com/advice';
 const id = document.getElementById('advice');
 const advice = document.getElementById('advice-text');
 
-fetch(apiUrl)
-.then((response)=> response.json())
-.then((data)=> {
-    
-    const  adviceId  = data.slip.id;
-    const  adviceText  = data.slip.advice;
+setInterval(() => {
+    fetch(apiUrl)
+    .then((response)=> response.json())
+    .then((data)=> {
+        
+        const  adviceId  = data.slip.id;
+        const  adviceText  = data.slip.advice;
 
-    id.textContent = `Advice #${adviceId}`
-    advice.innerText = `${adviceText}`
-    
-})
-.catch((error)=> {
-    console.log(error.message)
-})
+        id.textContent = `Advice #${adviceId}`
+        advice.innerText = `${adviceText}`
+        
+    })
+    .catch((error)=> {
+        console.log(error.message)
+    })
+},10000 );
